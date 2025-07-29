7fa15c1549c38dd9538a33af4b7c6bde00a20e50
kevincolorado45378 <coloradok02@gmail.com>
2025-07-29 16:46:13 -0500

    commit

--- parkingNFS frontend/backend/usuario/migrations/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Facturacion
-
-
-class FacturacionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Facturacion
-        fields = '__all__' 
